f6559b188262b760c1a86d5312a42ae02c63347c
Amandubey211 <109097090+Amandubey211@users.noreply.github.com>
2024-01-31 16:17:52 +0530

    portfolio server live

---
+++ Index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import DBConnect from "./Data_Base/DB.js";
+import FormRouter from "./MVC_/Routes/FormRouter.js";
+import cors from "cors";
+// import path from "path";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+// app.use(express.static(path.join(__dirname,"")))
+
+app.use("/api", FormRouter);
+
+app.get("/", (req, res) => {
+  res.send("hii aman");
+});
+
+// app.get("*",function(req,res){
+//   res.sendFile(path.join(__dirname,"./Client/"))
+// })
+DBConnect();
+app.listen(5000, () => {
+  console.log(`server connection successfull.`);
+});
